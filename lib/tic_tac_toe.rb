70a055b775f4c2dd9c27314341da5504e0fa94bf
Christopher Dent <christopherdent01@gmail.com>
2019-08-28 01:26:03 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -86,7 +86,24 @@ end
     end 
   end 
   
-  
+    
+  def turn
+    display_board
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    
+    index = input_to_index(user_input)  
+    
+      if !valid_move?(index)
+        turn 
+      end 
+      move(index, current_player)
+        
+      
+      end
+      
+
+  end
 
 def turn
   display_board
