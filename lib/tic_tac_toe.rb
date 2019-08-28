a601022481523ec0f08a33f69b7f15ef8c9e8b6e
Christopher Dent <christopherdent01@gmail.com>
2019-08-28 00:28:28 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,114 @@
+class TicTacToe
+  
+  
+  @board = Array.new(9, "")
+  
+  def initialize
+    @board
+  end 
+  
+  
+  
+class TicTacToe
+  
+  
+  
+  
+  def initialize
+    
+    
+   @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+   
+   
+  end 
+  
+  WIN_COMBINATIONS = [[0, 1, 2],
+                      [3, 4, 5], 
+                      [6, 7, 8],
+                      [0, 4, 8],
+                      [2, 4, 6],
+                      [0, 3, 6],
+                      [1, 4, 7],
+                      [2, 5, 8]]
+                      
+  def display_board
+    
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end 
+
+  
+  
+  def input_to_index(user_input)
+  user_input.to_i - 1
+  end 
+  
+  def move(index, token="X")
+    
+    @board[index] = token
+  end 
+  
+  
+  def position_taken?(index)
+    if @board[index] == "X" 
+      true
+    elsif @board[index] == "O"
+      true 
+    else
+      false 
+    end 
+  end 
+  
+  
+  def valid_move?(index)
+    if position_taken?(index)
+      false
+    elsif 
+      index > 8
+      false 
+    else 
+      true 
+    end
+  end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+def turn_count(board)
+ counter = 0
+ board.each do |spot|
+  if spot == "X" || spot == "O"
+      counter += 1
+    end
+  end 
+return counter
+end
+  
+  
+  
+  
+  
+  
+end 
