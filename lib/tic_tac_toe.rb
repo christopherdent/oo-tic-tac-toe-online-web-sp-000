9f0fdeae7e4343f36c20f85321faa5c80357aeb3
Christopher Dent <christopherdent01@gmail.com>
2019-08-28 00:30:52 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -75,7 +75,16 @@ class TicTacToe
   end 
   
   
-  
+    
+def turn_count(board)
+ counter = 0
+ board.each do |spot|
+  if spot == "X" || spot == "O"
+      counter += 1
+    end
+  end 
+return counter
+end
   
   
   
@@ -95,16 +104,7 @@ end
   
   
   
-  
-def turn_count(board)
- counter = 0
- board.each do |spot|
-  if spot == "X" || spot == "O"
-      counter += 1
-    end
-  end 
-return counter
-end
+
   
   
   
