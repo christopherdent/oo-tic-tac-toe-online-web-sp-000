1315e934fee3e1d2be3522788f92d9371f6fede6
Christopher Dent <christopherdent01@gmail.com>
2019-08-28 01:17:12 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -91,8 +91,8 @@ end
 def turn
   puts "Please enter 1-9:"
   user_input = gets.strip
-     user_input = gets.strip
-   index = input_to_index(user_input)
+   input_to_index(user_input)  
+   
    if valid_move?(user_input)
       move(user_input, current_player)
       else
