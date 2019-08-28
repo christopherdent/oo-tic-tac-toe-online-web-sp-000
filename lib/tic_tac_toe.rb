04f2a74be450702bb63b300b85567b36dc041fc2
Christopher Dent <christopherdent01@gmail.com>
2019-08-28 00:49:36 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -89,7 +89,9 @@ end
   def turn
     puts "Where would you like to go?"
     user_input = gets.strip 
+    
     index = input_to_index(user_input) 
+    
     if valid_move?(index)
       @board[current_player]
       puts @board 
